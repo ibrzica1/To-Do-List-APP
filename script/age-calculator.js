4dc94b19adee9a77154db1441dac55f4c386d063
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-05-04 16:14:24 +0100

    Age Calculator finished

--- script/age-calculator.js
+++ script/age-calculator.js
@@ -3,15 +3,23 @@ let result = document.getElementById("result");
 let year = 0;
 let month = 0;
 let day = 0;
+let restYear = 0;
+let restMonth = 0;
 
 
 
 document.querySelector("button").addEventListener("click", () => {
   inputDate = new Date(inputDate.value);
   let dateNow = new Date();
-  year = dateNow.getFullYear() - inputDate.getFullYear();
-  month = dateNow.getMonth() - inputDate.getMonth();
-  day = dateNow.getDay() - inputDate.getDay();
+  let daysTotal = Math.floor((dateNow - inputDate)/(1000*60*60*24));
+  year = Math.floor(daysTotal/365);
+  restYear = daysTotal % 365;
+  month = Math.floor(restYear / 30);
+  day = restYear % 30;
+  
+  console.log(daysTotal);
+  console.log(restYear);
+  console.log(month);
   
 result.innerHTML = `You are ${year} years, ${month} months and ${day} days old`
 });
