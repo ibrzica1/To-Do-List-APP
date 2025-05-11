4724ca5b2a3a1e06d18150bdaa7ca0790bce2f97
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-05-11 07:03:09 +0100

    Calculator App

--- script/calculator.js
+++ script/calculator.js
@@ -1,12 +1,28 @@
-let display = document.querySelector(".display").innerHTML;
+let display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".button");
+const calculators = document.querySelectorAll(".button-calculators");
 let string = "";
 console.log(display);
 buttons.forEach(button => {
   button.addEventListener("click", () =>{
-    let value = button.innerHTML;
-    string += value;
-    console.log(display);
-    display.innerHTML = string;
+    if(button.innerHTML == 0 & string == "") {
+
+    }
+    else {
+      string += button.innerHTML;
+      console.log(string);
+      display.innerHTML = string;
+    }
   })
 })
+
+calculators.forEach(calculator => {
+  calculator.addEventListener('click', () => {
+    let number = parseFloat(string);
+    string += calculator.innerHTML;
+    display.innerHTML = string;
+    console.log(number);
+  })
+})
+
+
